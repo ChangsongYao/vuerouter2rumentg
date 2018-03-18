4bcac3931fd459cc8fe6dbb84ca660f7116328df
Hidetada-komatu <2543144515@qq.com>
2018-03-18 13:12:28 +0800

    2.5 使用组件属性传递数据

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,18 @@
 <template>
   <div id="app" v-cloak>
-    <div class="demo" v-for="demo in demos">
-      <h4>[{{demo.name}}] {{demo.pattern}}</h4>
-      <ul>
-        <li v-for="test in demo.tests">
-          <router-link :to="test">{{test}}</router-link>
-        </li>
-      </ul>
-    </div>
-    <div class="content">
-      <router-view></router-view>
-    </div>
+    <nav>
+      <router-link to="/?username=WHOAMI">首页</router-link>
+      <router-link to="/shop?id=1&message=你好，欢迎光临1号店&banner=static/shop1-banner.jpg">1号店</router-link>
+      <router-link to="/shop?id=2&message=2号店欢迎你的光临&banner=static/shop2-banner.jpg">2号店</router-link>
+      <router-link to="/about">关于</router-link>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'App',
-    data:function () {
-      return {
-        demos:[
-          {
-            name: '命名参数',
-            pattern: '/named-param/blog/:id',
-            tests: [
-              '/named-param/blog/123',
-              '/named-param/blog/abc'
-            ]
-          },
-          {
-            name: '定制命名参数',
-            pattern: '/custom-named-param/blog/:id(\\d+)',
-            tests:[
-              '/custom-named-param/blog/123',
-              '/custom-named-param/blog/abc'
-            ]
-          },
-          {
-            name: '可选参数',
-            pattern: '/optional-param/blog/:id?',
-            tests: [
-              '/optional-param/blog',
-              '/optional-param/blog/787878'
-            ]
-          },
-          {
-            name: '零或多个参数',
-            pattern: '/zero-or-more-param/fileman/:path*',
-            tests: [
-              '/zero-or-more-param/fileman/',
-              '/zero-or-more-param/fileman/root/var/nginx/README'
-            ]
-          },
-          {
-            name: '一个或多个参数',
-            pattern: '/one-or-more-param/fileman/:path+',
-            tests: [
-              '/one-or-more-param/fileman/',
-              '/one-or-more-param/fileman/root/var/nginx/README'
-            ]
-          },
-          {
-            name: '匿名参数',
-            pattern: '/anonymous-param/card/(\\d+)',
-            tests:[
-              '/anonymous-param/card/123',
-              '/anonymous-param/card/abc'
-            ]
-          },
-          {
-            name: '任意匹配',
-            pattern: '/anonymous-param/card/*',
-            tests:[
-              '/asterisk-param/card/hubwiz/a/b',
-              '/asterisk-param/card/ezsoft'
-            ]
-          }
-        ]
-      }
-    }
+    name: 'App'
   }
 </script>
 
@@ -97,17 +31,18 @@
     0%{width:0%}
     100%{width:90%}
   }
-  .content{
-    position:fixed;
-    right:10px;
-    top:10px;
-    border:1px solid #000;
-    width:300px;
-    height:200px;
-    box-shadow:5px 5px 5px 2px rgba(0,0,0,.2);
+  a{
+    text-decoration: none;
+    color:black;
+    padding:10px 20px;
+    text-align:center;
   }
-  pre{
-    font-family:Consolas;
+  .router-link-exact-active{
+    color: bold;
+    border-bottom: 2px solid red;
+  }
+  img.banner{
+    width:100%;
   }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -11,21 +11,35 @@ Vue.config.productionTip = false
 
 Vue.use(VueRouter);
 
-const EzHome = {template:'<h4>渲染结果看这里</h4>'}
-const EzBlog = {template:'<h4>BLOG {{$route.params.id}}</h4>'}
-const EzFileMan = {template:'<h4>FILE {{$route.params.path}}</h4>}'}
-const EzCard = {template:'<h4>CARD {{$route.params[0]}}</h4>'}
+const EzHome = {
+  props:{
+    user: {
+      default:'Anonymous'
+    }
+  },
+  template:'<h1>HELLO, {{user}}</h1>'
+}
+const EzShop = {
+  props:['id','message','banner'],
+  template:`
+    <div class="shop">
+      <h1>SHOP {{id}}#</h1>
+      <p>{{message}}</p>
+      <img :src="banner" class="banner">
+    </div>
+  `,
+}
+const EzAbout = {template:'<h1>ABOUT</h1>'}
 
 const router = new VueRouter({
   routes:[
     {path:'/',component:EzHome},
-    {path:'/named-param/blog/:id',component:EzBlog},
-    {path:'/custom-named-param/blog/:id(\\d+)',component:EzBlog},
-    {path:'/optional-param/blog/:id?',component:EzBlog},
-    {path:'/zero-or-more-param/fileman/:path*',component:EzFileMan},
-    {path:'/one-or-more-param/fileman/:path+',component:EzFileMan},
-    {path:'/anonymous-param/card/(\\d+)',component:EzCard},
-    {path:'/asterisk-param/card/*',component:EzCard}
+    {
+      path:'/shop',
+      component:EzShop,
+      props: route => route.query
+    },
+    {path:'/about',component:EzAbout}
   ]
 })
 
