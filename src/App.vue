3065ea6e74083de57596aa176adb9b7f392a283f
Hidetada-komatu <2543144515@qq.com>
2018-03-18 11:55:11 +0800

    1.2 Hello, VueRouter

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div id="app" v-cloak>
+    <router-link to="/">首页</router-link>
+    <router-link to="/about">关于</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App'
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  [v-cloak] {
+    height: 2px;
+    overflow: hidden;
+  }
+
+  [v-cloak]:before {
+    content: ' ';
+    display: block;
+    border-bottom: 2px solid red;
+    animation: progress 2s infinite;
+  }
+
+  @keyframes progress {
+    0% {
+      width: 0%
+    }
+    100% {
+      width: 90%
+    }
+  }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,28 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
+Vue.use(VueRouter);
+
+const EzHome = {template: '<h1>HOME</h1>'}
+const EzAbout = {template: '<h1>ABOUT</h1>'}
+
+const router = new VueRouter({
+  routes: [
+    {path: '/', component: EzHome},
+    {path: '/about', component: EzAbout}
+  ]
+})
+
+
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
