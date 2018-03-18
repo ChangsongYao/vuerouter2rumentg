af246da2b149a8536f43f4bece6094b100b1525d
Hidetada-komatu <2543144515@qq.com>
2018-03-18 14:02:55 +0800

    6.4 活动链接的样式类及判别规则

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="app" v-cloak>
-    <nav>
-      <router-link to="/" tag="img" src="static/matrix-logo.jpg" alt="home">首页</router-link>
-      <router-link to="/shop/1">1号店</router-link>
-      <router-link to="/shop/2">2号店</router-link>
+    <nav class="header">
+      <router-link to="/" exact>首页</router-link>
+      <router-link to="/blog">博客</router-link>
       <router-link to="/about">关于</router-link>
     </nav>
     <router-view></router-view>
@@ -13,45 +12,63 @@
 <script>
   import VueRouter from 'vue-router'
   const EzHome = {template:'<h1>HOME</h1>'}
-  const EzShop = {template:'<h1>SHOP {{$route.params.id}}#</h1>'}
+
+  const EzBlogs = {
+    data(){
+      return {
+        blogs:[
+          {title:'blog 1#',content:'content of blog 1#...'},
+          {title:'blog 2#',content:'content of blog 2#...'},
+          {title:'blog 3#',content:'content of blog 3#...'}]
+      }
+    },
+    template:`
+    <div class="blogs">
+      <nav>
+        <router-link v-for="(blog,idx) in blogs"
+          :to="'/blog/' + idx"
+          active-class="blog-active"
+          exact-active-class="blog-exact-active">
+          {{blog.title}}
+        </router-link>
+      </nav>
+      <router-view></router-view>
+    </div>
+  `
+  }
+
+  const EzBlog = {
+    template:`
+    <article>
+  	  <h1>{{blog.title}}</h1>
+      <p>{{blog.content}}</p>
+    </article>
+    `,
+    computed:{
+      blog(){ return this.$parent.blogs[+this.$route.params.idx] }
+    }
+  }
+
+
   const EzAbout = {template:'<h1>ABOUT</h1>'}
 
   const router = new VueRouter({
     routes:[
-      {path:'/',component:EzHome,name:'home'},
-      {path:'/shop/:id',component:EzShop,name:'shop'},
-      {path:'/about',component:EzAbout,name:'about'}
+      {path:'/',component:EzHome},
+      {
+        path:'/blog',
+        component:EzBlogs,
+        children:[
+          {path:':idx',component:EzBlog}
+        ]
+      },
+      {path:'/about',component:EzAbout}
     ]
   })
 
-  const EzLink = {
-    props:['to'],
-    render(h){
-      const router = this.$router
-      const route = this.$route
-
-      const data = {
-        class:{
-          'router-link-exact-active': route.path === this.to
-        },
-        attrs:{
-          href: this.to
-        },
-        on:{
-          click: evt => {
-            evt.preventDefault()
-            router.push(this.to)
-          }
-        }
-      }
-      return h('a',data,this.$slots.default)
-    }
-  }
-
   export default {
     name: 'App',
-    router,
-    components:{EzLink}
+    router
   }
 </script>
 
@@ -70,21 +87,49 @@
     0%{width:0%}
     100%{width:90%}
   }
-  nav > *{
-    vertical-align:middle;
-    display:inline-block;
-    border:none;
-    padding:10px 20px;
-    cursor:pointer;
+  nav.header{
+    padding:15px;
+    background:#f0f0f0;
   }
-  nav > a{
+  nav.header a{
     text-decoration: none;
     color:black;
+    padding:10px 20px;
     text-align:center;
   }
-  nav > *.router-link-exact-active{
+  nav.header .router-link-active{
     color: bold;
     border-bottom: 2px solid red;
+  }
+  div.blogs{
+    display:flex;
+    margin-top:10px;
+  }
+  .blogs nav{
+    width:150px;
+    background:#f0f0f0;
+  }
+  .blogs article{
+    padding: 0px 10px;
+    flex:1;
+  }
+  .blogs a{
+    display:block;
+    text-decoration: none;
+    color:black;
+    padding:10px 20px;
+    text-align:center;
+  }
+  .blogs a:hover{
+    background:#777;
+    color:white;
+  }
+  .blogs a.blog-exact-active{
+    background:blue;
+    color:white;
+  }
+  pre{
+    font-family:Consolas;
   }
 
 
